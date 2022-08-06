0a6b5ef6a7edb965e258d755f39437be8f38a770
Sanket_Jagdale <sanketsj00900@gmail.com>
2022-08-06 23:08:23 +0530

    final commit

---
+++ Code/script.js
@@ -0,0 +1,58 @@
+let apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+function newQuote() {
+  showLoadingSpinner();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Uknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+  hideLoadingSpinner();
+}
+
+async function getQuotes() {
+  showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", getQuotes);
+twitterBtn.addEventListener("click", tweetQuote);
+
+getQuotes();
